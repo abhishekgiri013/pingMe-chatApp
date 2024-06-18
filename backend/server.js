3a9cf5554419fe2856e5398b59924cdba7b00f65
abhishekGiri <abhishekgiri013@gmail.com>
2024-06-19 03:51:26 +0530

    4th part completed -socket

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,35 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-import cookieParser from "cookie-parser";
-import express from "express";
-import dotenv from "dotenv"
-
-import connectToMongoDB from "./db/connecttoMongoDB.js";
-
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-// app.get("/",(req,res)=> {
-//      res.send("Hello World xx");
-// });
+const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
-    connectToMongoDB();
-    console.log(`Server Running on port 5000 ${PORT}`)
-    });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
