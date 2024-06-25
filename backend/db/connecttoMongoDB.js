90a3119df8bae812b6f52a9ec5dc9cace984eda8
abhishekGiri <abhishekgiri013@gmail.com>
2024-06-26 03:11:07 +0530

    N

--- backend/db/connecttoMongoDB.js
+++ backend/db/connecttoMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB  =async() =>{
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI)
+        await mongoose.connect(process.env.MONGO_DB_URI);
         console.log("connected to MongoDB");
     } catch (error) {
         console.log("Error conncecting to MongoDB", error.message);
